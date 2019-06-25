4fd4dfdf4ca0f47a1649ef8106173f4154c5f57b
Rajat Cambo <rajatcambo7@gmail.com>
2019-06-26 00:30:59 +0530

    added javascript

--- Color Game/script.js
+++ Color Game/script.js
@@ -1,7 +1,9 @@
 let colors=["rgb(255, 0, 0)", "rgb(0, 255, 0)", "rgb(0, 0, 255)", "rgb(255, 255, 0)", "rgb(0, 255, 255)", "rgb(255, 0, 255)"]
+let bgcolor=["#85144b", "#001f3f", "#0074D9", "#39CCCC", "#3D9970", "#FFDC00", "#B10DC9"]
 let flag=true;
 let ra=Math.floor((Math.random() * 6) + 1);
 let blocks=document.getElementsByClassName("colorb")
+let clickcount=0
 var randcolor=function(){
     let r=Math.floor((Math.random() * 256) + 1);
     let b=Math.floor((Math.random() * 256) + 1);
@@ -12,6 +14,8 @@ let caar=randcolor();
 let constcolor="rgb("+caar[0]+", "+caar[1]+", "+caar[2]+")";
 let dynamic=document.getElementById('dynamic')
 dynamic.innerHTML=constcolor;
+
+document.getElementById('title').style.backgroundColor=bgcolor[Math.floor((Math.random() * 7) + 1)]
 for(var i=0;i<blocks.length;i++){
     if (i==ra && flag) {
         blocks[i].style.backgroundColor=constcolor;
@@ -21,3 +25,27 @@ for(var i=0;i<blocks.length;i++){
         blocks[i].style.backgroundColor=colors[i]; 
     }
 }
+
+let chances=document.getElementById('chances')
+
+for(let k=0;k<blocks.length;k++){
+    blocks[k].onclick=function(){
+        if(this.style.backgroundColor==constcolor){
+            alert('found')
+            document.location.reload(true)
+        }
+        else{
+            this.remove()
+        }
+        clickcount++
+        if (clickcount<3) {
+            chances.innerHTML=(3-clickcount);
+        }
+        else {
+            chances.innerHTML=0;
+            alert('you lose')
+            document.location.reload(true)    
+        }
+    }
+}
+
